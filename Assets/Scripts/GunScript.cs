4e9f49d42a1bbe49e5d62a3cd512e1e5c3bc584e
Alexey Dyakonchuk <C00Lman041193@gmail.com>
2018-02-02 18:10:05 +0300

    attemt to script text

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+    public GameObject text;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, -Input.GetAxis("Mouse X")) * Time.deltaTime * 10.0f);
+        //text.GetComponent();
+	}
+}
